82d75d62e9096d3ce71d6f3097f4632ed0277a35
Surge Brian Mutugi <160673325+daslime@users.noreply.github.com>
2025-06-07 18:48:24 +0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -64,7 +64,7 @@ export default function Register() {
       {/* Decorative Images */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
         <img
-          src="/src/assets/happy-black-male-doctor-pointing-empty-space-presenting-product.jpg"
+          src="https://img.freepik.com/free-photo/happy-black-male-doctor-pointing-empty-space-presenting-product_1262-12349.jpg?t=st=1749311091~exp=1749314691~hmac=077b444b397db6e69a8f76e1a91a6cf7f76d6a27d80ad645568310adff2edbe7&w=1380"
           alt="Doctor Presenting"
           className="absolute top-0 right-0 w-full h-full object-cover opacity-30 transform rotate-6 scale-110"
         />
